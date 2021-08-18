88349e18ab77d6624bea4c20ec75a007452794f1
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2021-08-18 22:25:44 +1200

    audio can be converted to string and sent over the internet

--- public/index.js
+++ public/index.js
@@ -114,3 +114,31 @@ function refreshRecordings(){
   })
 }
 
+
+
+function makeStrOfAudio(recording){
+  let b ="";recording.audioBlob.arrayBuffer().then(a =>{console.log(a); b = a})
+  return ab2str(b)
+}
+
+function ab2str(buf) {
+  return String.fromCharCode.apply(null, new Uint8Array(buf));
+}
+
+
+function makeAudioOfString(audiostr){
+  let c = str2ab(audiostr)
+  let audio2 = document.createElement("audio");
+  const blob = new Blob([d], { type: "audio/wav" });
+  audio2.src = window.URL.createObjectURL(blob);
+  audio2.play()
+}
+
+function str2ab(str) {
+  var buf = new ArrayBuffer(str.length); // 2 bytes for each char
+  var bufView = new Uint8Array(buf);
+  for (var i=0, strLen=str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
+  }
+  return buf;
+}
